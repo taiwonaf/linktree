c6738d8699a9f67d3e8083401ffd1b20ec93cc89
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2022-10-22 23:51:17 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Links from './components/Links';
+import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
       <Links />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import './styles/footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+        <div className="footer-wrapper">
+            <div className="zuri">
+                <img src="/images/zuri.png" alt="" />
+            </div>
+            <div className="middle-text">
+                <p>
+                    HNG Internship 9 Frontend Task
+                </p>
+            </div>
+            <div className="i4g">
+                <img src="/images/I4g.png" alt="" />
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
