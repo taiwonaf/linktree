7d035bcd086e80b6b0ae8f18cac675d2d8326b36
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2022-10-27 12:46:06 +0100

    Added a new type of sub text using span

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,9 @@ import './styles/button.css';
 const Button = ({item}) => {
     const {name, url, id, alt} = item
   return (
-    <a href={url} className='button' id={id} title={alt}>
+    <a href={url} className='button' id={id} >
         {name}
+        {alt && <span>{alt}</span>}
     </a>
   )
 }
