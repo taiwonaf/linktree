82ad4f240daf42785b16560dde5906f3a090094d
Nafiu Taiwo <nafiutaiwosolomon@gmail.com>
2022-10-27 13:58:04 +0100

    Added link to my profile

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,10 +5,14 @@ const Socials = () => {
     <div className='socials'>
         <div className="socials-wrapper">
             <div className="slack">
+              <a href="#">
                 <img src="/images/slack.png" alt="" />
+              </a>
             </div>
             <div className="github">
+              <a href="https://github.com/taiwonaf">
                 <img src="/images/github.png" alt="" />
+              </a>
             </div>
         </div>
     </div>
