b720ad68134f9b63d913c20c98c244ff30a758ab
Taiwonaf <nafiutaiwosolomon@gmail.com>
2022-11-04 14:05:32 +0100

    Style: css animations and state styling

--- src/components/contact/Checkbox.jsx
+++ src/components/contact/Checkbox.jsx
@@ -1,8 +1,18 @@
 import './styles/Checkbox.css';
+import { useState} from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 const Checkbox = () => {
+    const [check, setCheck] = useState(false);
+    const changeCheck = () => {
+        setCheck(prev => !prev)
+    }
   return (
     <div className='checkbox'>
-        <div className="check"></div>
+        <div className={!check ? "check check-blink" : "check checked"} onClick={changeCheck} >
+            {check && <FontAwesomeIcon icon={faCheck} size='sm' />}
+        </div>
         <p>You agree to providing your data to Taiwo who may contact you.</p>
     </div>
   )
